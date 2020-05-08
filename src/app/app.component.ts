0a3dcd3d0e0606730c1a9eb418e53202157fdaa9
Micah Johnson <mjohnson@teleflora.org>
2020-05-08 13:55:43 -0500

    init upload of Cards angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imgUrl: 'assets/tree.jpeg',
+      username: '@nature',
+      content: 'Saw this awesome tree today'
+    },
+    {
+      title: 'Tall mountain',
+      imgUrl: 'assets/mountain.jpeg',
+      username: '@hiker',
+      content: 'Went hiking today in Arbuckle mountain'
+    },
+    {
+      title: 'Cool bike',
+      imgUrl: 'assets/biking.jpeg',
+      username: '@biker',
+      content: 'Went riding today at Lake Hefner'
+    }
+  ]
 }
